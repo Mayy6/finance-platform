3df4172bd9fa160c0585019d277387c5a38e4de6
Mayy6 <xy551166@outlook.com>
2026-02-16 18:11:30 +1030

    delete transaction route

--- finance-platform/app/api/[[...route]]/route.ts
+++ finance-platform/app/api/[[...route]]/route.ts
@@ -3,14 +3,12 @@ import { handle } from "hono/vercel";
 import accounts from "./accounts";
 import categories from "./categories";
 import { tr } from "zod/v4/locales";
-import  transactions  from "./transactions";
 
 export const runtime = "edge";
 const app = new Hono().basePath("/api");
 const routes = app
 .route("/accounts", accounts)
 .route("/categories", categories)
-.route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
